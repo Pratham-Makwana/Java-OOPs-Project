b1e2b5921cac8b75da32c88439d4dff10370986a
Pratham Makwana <makwanapratham13@gmail.com>
2024-04-04 22:48:22 +0530

    Hotel Reservation Using JDBC Database

--- HotelReservation-System/src/com/Pratham/HotelReservationSystem.java
+++ HotelReservation-System/src/com/Pratham/HotelReservationSystem.java
@@ -1,6 +1,5 @@
-package HotelReservationSystem;
+package com.Pratham;
 
-import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -95,29 +94,29 @@ public class HotelReservationSystem {
 
     private static void viewReservation(Connection connection, Scanner scanner) throws SQLException {
 
-            String sql = "SELECT reservation_id,guest_name,room_number,contact_number,reservation_date FROM reservations";
-            try (Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(sql)){
+        String sql = "SELECT reservation_id,guest_name,room_number,contact_number,reservation_date FROM reservations";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)){
 
 
-                System.out.println("Current Reservations:");
-                System.out.println("+----------------+-----------------+---------------+----------------------+-------------------------+");
-                System.out.println("| Reservation ID | Guest           | Room Number   | Contact Number      | Reservation Date        |");
-                System.out.println("+----------------+-----------------+---------------+----------------------+-------------------------+");
+            System.out.println("Current Reservations:");
+            System.out.println("+----------------+-----------------+---------------+----------------------+-------------------------+");
+            System.out.println("| Reservation ID | Guest           | Room Number   | Contact Number      | Reservation Date        |");
+            System.out.println("+----------------+-----------------+---------------+----------------------+-------------------------+");
 
-                while (resultSet.next()){
-                    int reservationId = resultSet.getInt("reservation_id");
-                    String guestName = resultSet.getString("guest_name");
-                    int roomNumber = resultSet.getInt("room_number");
-                    String contactNumber = resultSet.getString("contact_number");
-                    String reservationDate = resultSet.getTimestamp("reservation_date").toString();
+            while (resultSet.next()){
+                int reservationId = resultSet.getInt("reservation_id");
+                String guestName = resultSet.getString("guest_name");
+                int roomNumber = resultSet.getInt("room_number");
+                String contactNumber = resultSet.getString("contact_number");
+                String reservationDate = resultSet.getTimestamp("reservation_date").toString();
 
-                    // Format and display the reservation data in a table-like format
-                    System.out.printf("| %-14d | %-15s | %-13d | %-20s | %-19s   |\n",
-                            reservationId, guestName, roomNumber, contactNumber, reservationDate);
-                }
-                System.out.println("+----------------+-----------------+---------------+----------------------+-------------------------+");
+                // Format and display the reservation data in a table-like format
+                System.out.printf("| %-14d | %-15s | %-13d | %-20s | %-19s   |\n",
+                        reservationId, guestName, roomNumber, contactNumber, reservationDate);
             }
+            System.out.println("+----------------+-----------------+---------------+----------------------+-------------------------+");
+        }
     }
 
     private static void getRoomNumber(Connection connection, Scanner scanner) {
@@ -132,7 +131,7 @@ public class HotelReservationSystem {
                     " AND guest_name = '" + guestName + "'";
 
             try (Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql)){
+                 ResultSet resultSet = statement.executeQuery(sql)){
 
                 if (resultSet.next()){
                     int roomNumber = resultSet.getInt("room_number");
